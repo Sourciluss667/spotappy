134ef793b18438c0bb490576238cec4792384cfe
Sourciluss667 <qcormand@sourciluss.net>
2020-03-06 20:37:55 +0100

    First commit!

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="navbar">
+
+<nav class="uk-navbar-container uk-margin" uk-navbar>
+    <div class="uk-navbar-left">
+
+        <a class="uk-navbar-item uk-logo" :href="profileLink" target="_blank" style="color: #00cc99;">{{ username }}</a>
+
+        <div class="uk-navbar-item uk-navbar-right" style="width: 40%;">
+            <input class="uk-input uk-form-width-small" type="text" placeholder="Recherche..." style="width: 100%;" v-model="searching">
+        </div>
+
+    </div>
+</nav>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      searching: ''
+    }
+  },
+  props: {
+    username: String,
+    profileLink: String
+  },
+  watch: {
+    searching: function (val) {
+      if (val !== '') {
+        this.$parent.searchItem(val)
+      } else {
+        this.$parent.research = { }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
